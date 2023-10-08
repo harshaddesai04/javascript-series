4715e1f9fdd04a265f3c99b8bb53fdc1bf7440a5
Harshad Desai <123326532+harshaddesai04@users.noreply.github.com>
2023-10-08 03:02:23 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 14423
+let accountEmail= "harshaddesai0404@gmail.com"
+var accountpassword="123456"
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+accountcity = "Kolhapur"
+let accountstate;
+console.log(accountId)
+console.table([accountEmail,accountId,accountpassword,accountcity,accountstate])
